ce26fb64e59a702c67073a98e70dce514ebf81c8
Emilien Mottet <emilien.mottet@grenoble-inp.org>
2019-02-25 16:03:22 +0100

    end custom_set

--- erlang/custom-set/src/custom_set.erl
+++ erlang/custom-set/src/custom_set.erl
@@ -3,24 +3,35 @@
 -export([add/2, contains/2, difference/2, disjoint/2, empty/1, equal/2, from_list/1, intersection/2, subset/2,
 	 union/2]).
 
+add(Elem,Set)  ->
+    case contains(Elem,Set) of
+        true -> Set;
+        false -> Set ++ [Elem]
+    end.
 
-add(_Elem, _Set) -> undefined.
+contains(Elem, Set) ->
+    lists:member(Elem, Set).
 
-contains(_Elem, _Set) -> undefined.
+difference(Set1, Set2) ->
+    [E || E <- Set1 , not contains(E,Set2)].
 
-difference(_Set1, _Set2) -> undefined.
+disjoint(Set1, Set2) ->
+    lists:all(fun (X) -> not contains(X,Set2)  end, Set1).
 
-disjoint(_Set1, _Set2) -> undefined.
-
-empty(_Set) -> undefined.
+empty(Set) when length(Set)==0 -> true;
+empty(_Set) -> false.
 
 equal(Set1, Set2) ->
-    Set1 == Set2.
+    lists:usort(Set1) == lists:usort(Set2).
 
-from_list(_List) -> undefined.
+from_list(List) ->
+    lists:usort(List).
 
-intersection(_Set1, _Set2) -> undefined.
+intersection(Set1, Set2) ->
+    [E || E <- Set2 , contains(E,Set1) ].
 
-subset(_Set1, _Set2) -> undefined.
+subset(Set1, Set2) ->
+    lists:all(fun (X) -> contains(X,Set2)  end, Set1).
 
-union(_Set1, _Set2) -> undefined.
+union(Set1, Set2) ->
+    lists:foldl(fun(X, Set) -> add(X,Set) end, Set1, Set2).

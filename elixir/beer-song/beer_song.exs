19d4666354f78a3418d2fdba9be8824211381361
Emilien Mottet <emilien.mottet@grenoble-inp.org>
2019-02-28 21:05:52 +0100

    beer song done

--- elixir/beer-song/beer_song.exs
+++ elixir/beer-song/beer_song.exs
@@ -2,9 +2,49 @@ defmodule BeerSong do
   @doc """
   Get a single verse of the beer song
   """
+
+  defp numero(number) when number == 0 do
+    "No more"
+  end
+
+  defp numero(number) do
+    to_string(rem(number + 100, 100))
+  end
+
+  defp bottle_s(number) when number != 1 do
+    "bottles"
+  end
+
+  defp bottle_s(_) do
+    "bottle"
+  end
+
+  defp bottle(number) do
+    numero(number) <> " " <> bottle_s(number)
+  end
+
+  defp the_bottle(number) when number > 1 do
+    "one"
+  end
+
+  defp the_bottle(_) do
+    "it"
+  end
+
+  defp what_do_you_do(number) when number >= 1 do
+    "Take #{the_bottle(number)} down and pass it around"
+  end
+
+  defp what_do_you_do(_) do
+    "Go to the store and buy some more"
+  end
+
   @spec verse(integer) :: String.t()
   def verse(number) do
-    # Your implementation here...
+    """
+    #{bottle(number)} of beer on the wall, #{String.downcase(bottle(number))} of beer.
+    #{what_do_you_do(number)}, #{String.downcase(bottle(number - 1))} of beer on the wall.
+    """
   end
 
   @doc """
@@ -12,6 +52,14 @@ defmodule BeerSong do
   """
   @spec lyrics(Range.t()) :: String.t()
   def lyrics(range) do
-    # Your implementation here...
+    range
+    |> Enum.map(fn x ->
+      verse(x)
+    end)
+    |> Enum.join("\n")
+  end
+
+  def lyrics do
+    lyrics(99..0)
   end
 end

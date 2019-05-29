cc6f1dd4398b60f8d7b86b81c1357c67a15a47da
Emilien Mottet <emilien.mottet@grenoble-inp.org>
2019-05-29 20:52:33 +0200

    leap done emacs

---
+++ elisp/leap/leap.el
@@ -0,0 +1,14 @@
+;;; leap.el --- Leap exercise (exercism)
+
+;;; Commentary:
+
+;;; Code:
+
+(defun leap-year-p (year)
+  (or (zerop (mod year 400))
+      (and (zerop (mod year 4)) (not (zerop (mod year 100))))))
+
+;; ((zerop (mod year 4)) (not (zerop (mod year 100))))
+
+(provide 'leap)
+;;; leap.el ends here

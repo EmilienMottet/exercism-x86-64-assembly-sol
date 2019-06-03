da41c14da891c585a3ecd7206e7014f1f635c3cc
Emilien Mottet <emilien.mottet@grenoble-inp.org>
2019-06-03 23:10:56 +0200

    refactor java twofer

--- java/two-fer/src/main/java/Twofer.java
+++ java/two-fer/src/main/java/Twofer.java
@@ -1,5 +1,7 @@
+import java.util.Objects;
+
 class Twofer {
   String twofer(String name) {
-      return "One for " + (name != null ? name : "you") + ", one for me.";
+      return String.format("One for %s, one for me.", Objects.nonNull(name) ? name : "you");
   }
 }

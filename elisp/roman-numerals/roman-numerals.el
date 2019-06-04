45101298ad5b53fe20f85e5bfcf95d3bc2253db4
Emilien Mottet <emilien.mottet@grenoble-inp.org>
2019-06-05 00:17:16 +0200

    refactor roman elisp

--- elisp/roman-numerals/roman-numerals.el
+++ elisp/roman-numerals/roman-numerals.el
@@ -1,32 +1,16 @@
-;;; roman-numerals.el --- roman-numerals Exercise (exercism)
+;; roman-numerals.el --- roman-numerals Exercise (exercism)
 
 ;;; Commentary:
 
 ;;; Code:
 
-(defun to-roman-1 (number)
+(defun to-roman-base (number unit five decade)
   (cond
    ((zerop number) "")
-   ((< number 4) (make-string number ?I))
-   ((= number 4) "IV")
-   ((< number 9) (concat "V" (make-string (- number 5) ?I)))
-   ((= number 9) "IX")))
-
-(defun to-roman-10 (number)
-  (cond
-   ((zerop number) "")
-   ((< number 4) (make-string number ?X))
-   ((= number 4) "XL")
-   ((< number 9) (concat "L" (make-string (- number 5) ?X)))
-   ((= number 9) "XC")))
-
-(defun to-roman-100 (number)
-  (cond
-   ((zerop number) "")
-   ((< number 4) (make-string number ?C))
-   ((= number 4) "CD")
-   ((< number 9) (concat "D" (make-string (- number 5) ?C)))
-   ((= number 9) "CM")))
+   ((< number 4) (make-string number unit))
+   ((= number 4) (string unit five))
+   ((< number 9) (concat (string five) (make-string (- number 5) unit)))
+   ((= number 9) (string unit decade))))
 
 (defun to-roman-1000 (number)
   (cond
@@ -35,7 +19,11 @@
 
 
 (defun to-roman (number)
-  (concat (to-roman-1000 (/ (mod number 10000) 1000)) (to-roman-100 (/ (mod number 1000) 100)) (to-roman-10 (/ (mod number 100) 10)) (to-roman-1 (mod number 10))))
+  (concat
+   (to-roman-1000 (/ (mod number 10000) 1000))
+   (to-roman-base (/ (mod number 1000) 100) ?C ?D ?M)
+   (to-roman-base (/ (mod number 100) 10) ?X ?L ?C)
+   (to-roman-base (mod number 10 ) ?I ?V ?X)))
 
 
 (provide 'roman-numerals)

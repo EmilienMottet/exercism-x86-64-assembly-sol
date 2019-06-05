702ff2e230f29ba927f71bb9bb4d79adf16db15f
Emilien Mottet <emilien.mottet@grenoble-inp.org>
2019-06-06 01:39:12 +0200

    fix again pangram

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -1,23 +1,28 @@
 # frozen_string_literal: true
 
-# Pangram
-class Pangram
-  def self.pangram?(word)
-    word.pangram?
-  end
-end
-
-# Extend String class
-class String
+# module Language for Pangram
+module Language
   ALPHABET = %w[
     a z e r t y u i o p
     q s d f g h j k l m
     w x c v b n
   ].freeze
-  def pangram?
-    word = downcase
-    ALPHABET.all? do |l|
-      word.include?(l)
+
+  # Extend String class
+  refine String do
+    def pangram?
+      word = downcase
+      ALPHABET.all? do |l|
+        word.include?(l)
+      end
     end
+  end
+end
+
+# Pangram
+class Pangram
+  using Language
+  def self.pangram?(word)
+    word.pangram?
   end
 end

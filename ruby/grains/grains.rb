e482f7a78a671fcb6dd54c33571682912944ef0d
Emilien Mottet <emilien.mottet@grenoble-inp.org>
2019-06-07 22:08:28 +0200

    v0 grains ruby

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -1,5 +1,14 @@
+# frozen_string_literal: true
+
+# Grain
 class Grains
   def self.square(index)
-    index
+    raise ArgumentError if (index <= 0) || (index > 64)
+
+    2**(index - 1)
+  end
+
+  def self.total
+    (1..64).sum { |i| square(i) }
   end
 end

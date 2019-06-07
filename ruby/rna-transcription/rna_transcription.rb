91ae00bdc9fb487b557f1b496d8aae842ddd10e6
Emilien Mottet <emilien.mottet@grenoble-inp.org>
2019-06-07 22:00:31 +0200

    correct rna

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -5,6 +5,6 @@ class Complement
   CONVERT_MAP = { 'C' => 'G', 'G' => 'C', 'T' => 'A', 'A' => 'U' }.freeze
 
   def self.of_dna(dna)
-    dna.each_char.map { |e| CONVERT_MAP[e] }.sum('')
+    dna.each_char.sum('') { |e| CONVERT_MAP[e] }
   end
 end

91ba57942f8e3e960ec687086ad6dcbf04a0d810
Emilien Mottet <emilien.mottet@grenoble-inp.org>
2019-06-10 23:15:22 +0200

    refactor word count ruby

--- ruby/word-count/word_count.rb
+++ ruby/word-count/word_count.rb
@@ -6,14 +6,16 @@ class Phrase
     @phrase = phrase
   end
 
-  def self.split_words(words)
-    words.scan(/[\w]+[w']*[\w]*\b/)
-  end
-
   def word_count
-    Phrase.split_words(@phrase.downcase)
-          .each_with_object(Hash.new(0)) do |word, h|
+    split_words
+      .each_with_object(Hash.new(0)) do |word, h|
       h[word] += 1
     end
+  end
+
+  private
+
+  def split_words
+    @phrase.downcase.scan(/[\w]+[w']*[\w]*\b/)
   end
 end

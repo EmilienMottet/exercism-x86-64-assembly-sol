90c76900c89c31714d5418573c67a3133f8cfc9d
Emilien Mottet <emilien.mottet@grenoble-inp.org>
2019-06-10 18:31:29 +0200

    reformat binary elisp

--- elisp/binary/binary.el
+++ elisp/binary/binary.el
@@ -8,7 +8,11 @@
 (require 'seq)
 
 (defun to-decimal (binary)
-  (apply '+ (seq-map-indexed (lambda (val index) (* (lsh 1 index) (if (eq val 49) 1 0))) (reverse binary))))
+  (apply '+
+         (seq-map-indexed
+          (lambda (val index)
+            (* (lsh 1 index) (if (eq val ?1) 1 0)))
+          (reverse binary))))
 
 (provide 'binary)
 ;;; binary.el ends here

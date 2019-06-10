00860441f870c590824feb3e995bb272533acbbd
Emilien Mottet <emilien.mottet@grenoble-inp.org>
2019-06-10 18:42:35 +0200

    reformat ruby word count

--- ruby/word-count/word_count.rb
+++ ruby/word-count/word_count.rb
@@ -3,12 +3,17 @@
 # Phrase
 class Phrase
   def initialize(phrase)
-    @phrase = phrase.scan(/[\w]+[w']*[\w]*\b/)
+    @phrase = phrase
+  end
+
+  def self.split_words(words)
+    words.scan(/[\w]+[w']*[\w]*\b/)
   end
 
   def word_count
-    @phrase.each_with_object(Hash.new(0)) do |word, h|
-      h[word.downcase] += 1
+    Phrase.split_words(@phrase.downcase)
+          .each_with_object(Hash.new(0)) do |word, h|
+      h[word] += 1
     end
   end
 end

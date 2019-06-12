11d288ca211cbab0d4af338c5583cbaa024bef68
Emilien Mottet <emilien.mottet@grenoble-inp.org>
2019-06-12 02:13:22 +0200

    word count ok

--- elisp/word-count/word-count.el
+++ elisp/word-count/word-count.el
@@ -4,10 +4,10 @@
 
 ;;; Code:
 
+(require 'seq)
+
 (defun word-count (sentence)
-  (seq-map (lambda (list) '((2 . 1))
-             (seq-group-by #'string sentence))))
-  
+  (seq-map (lambda (elt) (cons (car elt) (length (cdr elt)))) (seq-group-by #'identity (sort (split-string (downcase sentence) "[^a-z0-9A-Z]" t) 'string<))))
 
 (provide 'word-count)
 ;;; word-count.el ends here

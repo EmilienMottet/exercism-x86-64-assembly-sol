352fb0759b74c581f7c6502597f34fa03021ee81
Emilien Mottet <emilien.mottet@grenoble-inp.org>
2019-06-12 02:14:37 +0200

    reformat code

--- elisp/word-count/word-count.el
+++ elisp/word-count/word-count.el
@@ -7,7 +7,14 @@
 (require 'seq)
 
 (defun word-count (sentence)
-  (seq-map (lambda (elt) (cons (car elt) (length (cdr elt)))) (seq-group-by #'identity (sort (split-string (downcase sentence) "[^a-z0-9A-Z]" t) 'string<))))
+  (seq-map
+   (lambda (elt)
+     (cons (car elt) (length (cdr elt))))
+   (seq-group-by #'identity
+                 (sort
+                  (split-string (downcase sentence)
+                                "[^a-z0-9A-Z]" t)
+                  'string<))))
 
 (provide 'word-count)
 ;;; word-count.el ends here

4e60de11b028051661b59104986a403a215d579a
Emilien Mottet <emilien.mottet@grenoble-inp.org>
2019-06-16 00:20:52 +0200

    raindrops elisp

---
+++ elisp/raindrops/raindrops.el
@@ -0,0 +1,32 @@
+;;; raindrops.el --- Raindrops (exercism)
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'cl-lib)
+(require 'subr-x)
+(require 'seq)
+
+
+(setq rain-converter #s(hash-table size 3
+                                   test equal
+                                   data (
+                                         3 "Pling"
+                                         5 "Plang"
+                                         7 "Plong"
+                                         )))
+
+
+(defun convert (n)
+  (let ((res
+         (mapconcat 'identity
+                    (seq-map
+                     (lambda (x) (if (equal (mod n x) 0) (gethash x rain-converter) ""))
+                     (hash-table-keys rain-converter)) "")))
+    (if (equal "" res) (number-to-string n) res)))
+
+
+
+(provide 'raindrops)
+;;; raindrops.el ends here

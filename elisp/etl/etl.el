755d8855276470303e190f6e3facc70d2cb0cbfb
Emilien Mottet <emilien.mottet@grenoble-inp.org>
2019-06-17 23:43:09 +0200

    etl elisp

--- elisp/etl/etl.el
+++ elisp/etl/etl.el
@@ -4,8 +4,23 @@
 
 ;;; Code:
 
+(require 'seq)
+(require 'subr-x)
+
 (defun etl (h_t)
-  (hash-table-keys h_t))
+  (seq-reduce
+    (lambda (table key)
+      (if (> key 0)
+          (seq-do
+           (lambda (value)
+             (if (stringp value)
+                 (puthash (downcase value) key table)
+               (error "value must be string")))
+           (gethash key h_t))
+          (error "key must be > 0"))
+      table)
+    (hash-table-keys h_t)
+    (make-hash-table :test 'equal)))
 
 (provide 'etl)
 ;;; etl.el ends here

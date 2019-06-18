610323ddf36775b8b43ec2a7bf612d893c39371f
Emilien Mottet <emilien.mottet@grenoble-inp.org>
2019-06-18 18:34:42 +0200

    series.erl done

--- erlang/series/src/series.erl
+++ erlang/series/src/series.erl
@@ -4,7 +4,7 @@
 
 slices(_,[]) ->
     erlang:error("empty series");
-slices(SliceLength,Series) when (SliceLength =< 0) or (length(Series) < SliceLength) ->
+slices(SliceLength,Series) when (SliceLength =< 0) orelse (length(Series) < SliceLength) ->
     erlang:error("bad SliceLength");
 slices(SliceLength, Series) ->
     [ lists:sublist(Series,Index,SliceLength) || Index <- lists:seq(1,length(Series) - SliceLength + 1)].

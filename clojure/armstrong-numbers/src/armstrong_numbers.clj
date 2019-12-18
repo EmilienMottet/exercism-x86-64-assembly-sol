5788f4d5eb5dfb5ff26e2496cb3576b4a1ee0ad4
Emilien Mottet <emilien.mottet@michelin.com>
2019-12-18 11:44:41 +0100

    delete comments

--- clojure/armstrong-numbers/src/armstrong_numbers.clj
+++ clojure/armstrong-numbers/src/armstrong_numbers.clj
@@ -8,14 +8,3 @@
                 (math/expt (Long/valueOf (str itm))
                            (count (str num))))
               (str num)))))
-
-;; (defn armstrong? [num]
-;;   (= num
-;;      (long
-;;       (reduce +
-;;               (map
-;;                (fn [itm]
-;;                  (Math/pow (Character/digit itm 10)
-;;                            (count (str num))))
-;;                (str num)))))
-;; )

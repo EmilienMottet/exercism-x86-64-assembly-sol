5f31c9fbe5cf401b737f705a3ef3ba1d24a8a683
EmilienMottet <emilien.mottet@grenoble-inp.org>
2020-08-28 23:29:32 +0200

    prime factors erlang

--- erlang/prime-factors/src/prime_factors.erl
+++ erlang/prime-factors/src/prime_factors.erl
@@ -2,5 +2,18 @@
 
 -export([factors/1]).
 
+factors(Target,[Candidate | Candidates ],Acc) when Target rem Candidate == 0 ->
+    factors(Target div Candidate,[Candidate | Candidates ],[ Candidate | Acc]);
+factors(Target,[_Candidate | Candidates],Acc) ->
+    factors(Target,Candidates,Acc);
+factors(1,[],Acc) ->
+    Acc;
+factors(Target,[],Acc) ->
+    [Target | Acc ].
 
-factors(_Value) -> undefined.
+factors(Value) when Value > 1 ->
+    Seq = lists:seq(2, trunc(math:sqrt(Value)) + 1),
+    factors(Value,Seq,[]);
+factors(1) ->
+    [].
+

010cbff2fdbc64922f74c6e20b712de334c7088a
EmilienMottet <emilien.mottet@grenoble-inp.org>
2020-08-28 16:16:50 +0200

    first submit change

--- elixir/change/lib/change.ex
+++ elixir/change/lib/change.ex
@@ -16,29 +16,38 @@ defmodule Change do
   """
 
   @spec generate(list, integer) :: {:ok, list} | {:error, String.t()}
+  def generate(_coins, 0) do
+    {:ok, []}
+  end
+
   def generate(coins, target) do
-    coins = coins |> Enum.sort() |> Enum.reverse()
-    case do_generate(coins, target, []) do
-      nil -> {:error, "cannot change"}
-      res -> {:ok, res}
-    end
+    coins = coins |> Enum.sort()
+    posibilities = Map.new(coins, fn coin -> {coin, [coin]} end)
+
+    do_generate(coins, target, posibilities, Map.get(posibilities, target, []))
   end
 
-  defguardp is_new_best?(current,candidate) when length(candidate) < length(current) or ( length(candidate) == length(current) and candidate < current )
-
-  def do_generate(_coins, 0, acc) do
-    acc |> Enum.sort()
-  end
-  def do_generate([], _target, _acc) do
-    nil
+  def do_generate(_, _, posibilities, _) when map_size(posibilities) == 0 do
+    {:error, "cannot change"}
   end
 
-  def do_generate(coins, target, result) do
-    result ++ coins
-    |> Enum.reduce([], fn coin, acc ->
-      count = div(target,coin)
-      acc ++ List.duplicate(coin, count)
-      do_generate(coins, target, acc)
-    end)
+  def do_generate(coins, target, posibilities, []) do
+    posibilities =
+      posibilities
+      |> Enum.flat_map(fn {total, posibility} ->
+        coins
+        |> Enum.flat_map(fn
+          coin when total + coin <= target ->
+            [{total + coin, [coin | posibility]}]
+
+          _ ->
+            []
+        end)
+      end)
+      |> Map.new()
+
+    do_generate(coins, target, posibilities, Map.get(posibilities, target, []))
   end
+
+  def do_generate(_, _, _, changes), do: {:ok, changes |> Enum.sort()}
 end

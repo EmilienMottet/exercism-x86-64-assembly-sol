f0066e4f8ee2805424317599567a13a6a8775a2b
EmilienMottet <emilien.mottet@grenoble-inp.org>
2020-09-01 09:19:33 +0200

    complete nucleotide elisp

--- emacs-lisp/nucleotide-count/nucleotide-count.el
+++ emacs-lisp/nucleotide-count/nucleotide-count.el
@@ -4,8 +4,18 @@
 
 ;;; Code:
 
-(defun nucleotide-count (nucleotide)
-  (seq-map (lambda (x) (string-to-char x)) '(?A ?C ?G ?T)))
+(setq nucleotides '(?A ?C ?G ?T))
+
+(defun nucleotide-count (args)
+  (if (not (seq-difference args nucleotides))
+      (mapcar (lambda (nucleotide)
+                (cons nucleotide
+                      (seq-count (lambda (n)
+                                   (eq n
+                                       nucleotide))
+                                 args)))
+              nucleotides)
+    (error "invalid nucleotide found")))
 
 (provide 'nucleotide-count)
 ;;; nucleotide-count.el ends here

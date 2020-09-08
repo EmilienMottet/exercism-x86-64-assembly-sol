2626671426e5e7f42165c5ac8a443a358ac346b4
EmilienMottet <emilien.mottet@grenoble-inp.org>
2020-09-08 17:20:01 +0200

    improve elixir gigasecond

--- elixir/gigasecond/lib/gigasecond.ex
+++ elixir/gigasecond/lib/gigasecond.ex
@@ -5,8 +5,8 @@ defmodule Gigasecond do
   @spec from({{pos_integer, pos_integer, pos_integer}, {pos_integer, pos_integer, pos_integer}}) ::
           :calendar.datetime()
 
-  def from(dt = {{year, month, day}, {hours, minutes, seconds}}) do
+  def from(erlang_date_time = {{_year, _month, _day}, {_hours, _minutes, _seconds}}) do
     gigasecond = :math.pow(10, 9) |> round
-    dt |> NaiveDateTime.from_erl!() |> NaiveDateTime.add(gigasecond) |> NaiveDateTime.to_erl()
+    erlang_date_time |> NaiveDateTime.from_erl!() |> NaiveDateTime.add(gigasecond) |> NaiveDateTime.to_erl()
   end
 end

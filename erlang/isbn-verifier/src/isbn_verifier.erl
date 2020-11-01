95eeafcecc7f21ad1cfab8b0748b08318b438c63
EmilienMottet <emilien.mottet@grenoble-inp.org>
2020-11-01 10:44:26 +0100

    Isbn erlang

--- erlang/isbn-verifier/src/isbn_verifier.erl
+++ erlang/isbn-verifier/src/isbn_verifier.erl
@@ -2,5 +2,24 @@
 
 -export([is_valid/1]).
 
+clean_isbn_key(Isbn) ->
+    [case X of $X -> 10; X -> X - 48 end || X <- Isbn, ( ( X >= $0 ) and ( X =< $9 ) ) or (X == $X)].
 
-is_valid(_Isbn) -> undefined.
+clean_isbn_body(Isbn) ->
+    [case X of $X -> 10; X -> X - 48 end || X <- Isbn, ( ( X >= $0 ) and ( X =< $9 ) )].
+
+compute_isbn(Isbn) ->
+    Zipped = lists:zipwith(fun(X, Y) -> X * Y end, Isbn, lists:seq(10,1,-1)),
+    (lists:sum(Zipped) rem 11) == 0.
+
+is_valid(Isbn) when length(Isbn) > 9 ->
+    Body = clean_isbn_body(lists:sublist(Isbn,length(Isbn) -1)),
+    Key = clean_isbn_key(lists:sublist(Isbn,length(Isbn), 1)),
+    case Body ++ Key of
+        CleanIsbn when length(CleanIsbn) == 10 ->
+            compute_isbn(CleanIsbn);
+        _Else ->
+            false
+        end;
+is_valid(_Isbn) ->
+    false.

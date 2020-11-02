a32e47772733131c1b2284327024b41e0ddf403c
EmilienMottet <emilien.mottet@grenoble-inp.org>
2020-11-02 09:19:06 +0100

    complete isbn

--- erlang/isbn-verifier/src/isbn_verifier.erl
+++ erlang/isbn-verifier/src/isbn_verifier.erl
@@ -2,24 +2,34 @@
 
 -export([is_valid/1]).
 
-clean_isbn_key(Isbn) ->
-    [case X of $X -> 10; X -> X - 48 end || X <- Isbn, ( ( X >= $0 ) and ( X =< $9 ) ) or (X == $X)].
+clean_isbn_key($X) ->
+    10;
+clean_isbn_key(X) ->
+    X - 48 .
 
 clean_isbn_body(Isbn) ->
-    [case X of $X -> 10; X -> X - 48 end || X <- Isbn, ( ( X >= $0 ) and ( X =< $9 ) )].
+    [X - 48 || X <- Isbn, X >= $0, X =< $9].
 
 compute_isbn(Isbn) ->
-    Zipped = lists:zipwith(fun(X, Y) -> X * Y end, Isbn, lists:seq(10,1,-1)),
+    Zipped = lists:zipwith(fun(X, Y) -> X * Y end, Isbn, lists:seq(1,10)),
     (lists:sum(Zipped) rem 11) == 0.
 
-is_valid(Isbn) when length(Isbn) > 9 ->
-    Body = clean_isbn_body(lists:sublist(Isbn,length(Isbn) -1)),
-    Key = clean_isbn_key(lists:sublist(Isbn,length(Isbn), 1)),
-    case Body ++ Key of
+isbn_check(Isbn) ->
+    [Key | Body] = lists:reverse(Isbn),
+    CleanBody = clean_isbn_body(Body),
+    CleanKey = clean_isbn_key(Key),
+    case [ CleanKey | CleanBody ] of
         CleanIsbn when length(CleanIsbn) == 10 ->
             compute_isbn(CleanIsbn);
         _Else ->
             false
-        end;
+    end.
+
+is_valid(Isbn) when length(Isbn) > 9 ->
+    try
+        isbn_check(Isbn)
+    catch
+        _ -> false
+    end;
 is_valid(_Isbn) ->
     false.

ec27735c729448e220261e6208f670dadc0ce43a
EmilienMottet <emilien.mottet@grenoble-inp.org>
2020-11-10 15:21:24 +0100

    erlang grade_school add spec

--- erlang/grade-school/src/grade_school.erl
+++ erlang/grade-school/src/grade_school.erl
@@ -1,6 +1,9 @@
 -module(grade_school).
 
 -export([add/3, get/2, get/1, new/0]).
+-export_type([school/0]).
+
+-opaque school() :: ID::reference().
 
 add(Name, Grade, School) ->
     put(School, [{Name, Grade} | erlang:get(School)]),
@@ -14,6 +17,7 @@ get(School) ->
     List = erlang:get(School),
     [Name || {Name, _Grade} <- List].
 
+-spec new() -> school().
 new() ->
     A = make_ref(),
     put(A, []),

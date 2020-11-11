f269dd683ecac25e5e1441bbbba5d1c41995766f
EmilienMottet <emilien.mottet@grenoble-inp.org>
2020-11-11 11:40:53 +0100

    erlang update word-count

--- erlang/word-count/src/word_count.erl
+++ erlang/word-count/src/word_count.erl
@@ -8,7 +8,12 @@ count_words(Sentence) ->
                                [unicode, global, {capture, all, list}]),
     CleanWordList = lists:append(WordList),
     lists:foldr(fun (Word, Map) ->
-                        maps:put(Word, maps:get(Word, Map, 0) + 1, Map)
+                        maps:update_with(Word,
+                                         fun (V) ->
+                                                 V + 1
+                                         end,
+                                         1,
+                                         Map)
                 end,
                 maps:new(),
                 CleanWordList).

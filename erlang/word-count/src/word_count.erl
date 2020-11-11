63a5422ce2ff7ce0d698b526ef9b70798dbef831
EmilienMottet <emilien.mottet@grenoble-inp.org>
2020-11-11 13:43:50 +0100

    erlang use rebar3 format

--- erlang/word-count/src/word_count.erl
+++ erlang/word-count/src/word_count.erl
@@ -3,18 +3,12 @@
 -export([count_words/1]).
 
 count_words(Sentence) ->
-    {match, WordList} = re:run(string:lowercase(Sentence),
-                               "[\\w]+[']*[\\w]*\\b",
-                               [unicode, global, {capture, all, list}]),
+    {match, WordList} =
+        re:run(
+            string:lowercase(Sentence),
+            "[\\w]+[']*[\\w]*\\b",
+            [unicode, global, {capture, all, list}]),
     CleanWordList = lists:append(WordList),
-    lists:foldr(fun (Word, Map) ->
-                        maps:update_with(Word,
-                                         fun (V) ->
-                                                 V + 1
-                                         end,
-                                         1,
-                                         Map)
-                end,
+    lists:foldr(fun(Word, Map) -> maps:update_with(Word, fun(V) -> V + 1 end, 1, Map) end,
                 maps:new(),
                 CleanWordList).
-

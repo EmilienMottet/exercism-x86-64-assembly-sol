54fcd5621474f42da5ecd4917043adfd91d346f2
EmilienMottet <emilien.mottet@grenoble-inp.org>
2020-11-13 16:48:56 +0100

    ruby fix resistor color duo

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 # Compute color on resistor
-module ResistorColorDuo
+class ResistorColorDuo
   @color_map = %w[black brown red orange yellow green blue violet grey white].to_enum.with_index.to_h
 
-  def value(colors)
-    colors.take(2).map { |c| @@color_map[c].to_s }.join.to_i
+  def self.value(colors)
+    colors.take(2).map { |c| @color_map[c] }.join.to_i
   end
 end

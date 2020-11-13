44bbac400ed37ddddd28e1e4ae500f833f4398a2
EmilienMottet <emilien.mottet@grenoble-inp.org>
2020-11-13 11:02:43 +0100

    erlang // letter freq

--- erlang/parallel-letter-frequency/src/parallel_letter_frequency.erl
+++ erlang/parallel-letter-frequency/src/parallel_letter_frequency.erl
@@ -6,19 +6,8 @@ freq(Str) ->
     lists:foldr(fun(X, Acc) -> dict:update_counter(X, 1, Acc) end, dict:new(), Str).
 
 dict(Strings) ->
-    Pid = spawn(fun() -> loop(dict:new()) end),
-    lists:foreach(fun(Str) -> Pid ! Str end, Strings),
-    Pid ! {get, self()},
-    receive
-        D ->
-            D
-    end.
-
-loop(D) ->
-    receive
-        {get, From} ->
-            From ! D;
-        String ->
-            ND = dict:merge(fun(_Key, V1, V2) -> V1 + V2 end, D, freq(String)),
-            loop(ND)
-    end.
+    S = self(),
+    Pids = [spawn(fun() -> S ! {self(), freq(Str)} end) || Str <- Strings],
+    lists:foldr(fun(D, Acc) -> dict:merge(fun(_Key, V1, V2) -> V1 + V2 end, D, Acc) end,
+                dict:new(),
+                lists:map(fun(Pid) -> receive {Pid, D} -> D end end, Pids)).

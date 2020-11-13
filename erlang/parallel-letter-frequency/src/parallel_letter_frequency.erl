82a3a7f2830065684bad6f9b1580704213570b6a
EmilienMottet <emilien.mottet@grenoble-inp.org>
2020-11-13 17:51:17 +0100

    erlang fail handled

--- erlang/parallel-letter-frequency/src/parallel_letter_frequency.erl
+++ erlang/parallel-letter-frequency/src/parallel_letter_frequency.erl
@@ -2,12 +2,23 @@
 
 -export([dict/1]).
 
-freq(Str) ->
-    lists:foldr(fun(X, Acc) -> dict:update_counter(X, 1, Acc) end, dict:new(), Str).
+freq(Str) when is_list(Str) ->
+    {ok, lists:foldr(fun(X, Acc) -> dict:update_counter(X, 1, Acc) end, dict:new(), Str)}.
 
 dict(Strings) ->
     S = self(),
-    Pids = [spawn(fun() -> S ! {self(), freq(Str)} end) || Str <- Strings],
+    process_flag(trap_exit, true),
+    Pids = [spawn_link(fun() -> S ! freq(Str) end) || Str <- Strings],
     lists:foldr(fun(D, Acc) -> dict:merge(fun(_Key, V1, V2) -> V1 + V2 end, D, Acc) end,
                 dict:new(),
-                lists:map(fun(Pid) -> receive {Pid, D} -> D end end, Pids)).
+                lists:map(fun(Pid) -> loop(Pid) end, Pids)).
+
+loop(Pid) ->
+    receive
+        {ok, D} ->
+            D;
+        {'EXIT', _Pid, normal} ->
+            loop(Pid);
+        {'EXIT', _FromPid, _Reason} ->
+            dict:new()
+    end.
--- erlang/parallel-letter-frequency/test/parallel_letter_frequency_tests.erl
+++ erlang/parallel-letter-frequency/test/parallel_letter_frequency_tests.erl
@@ -8,5 +8,10 @@ single_test() ->
   ?assert( lists:sort(Frequencies) =:= [{$a,1},{$d,1},{$s,1}] ).
 
 double_test() ->
-  Frequencies = dict:to_list( parallel_letter_frequency:dict(["asd", "asd"]) ),
+    Frequencies = dict:to_list( parallel_letter_frequency:dict(["asd", "asd"]) ),
+    io:format("freq:~p~n",[Frequencies]),
+    ?assert( lists:sort(Frequencies) =:= [{$a,2},{$d,2},{$s,2}] ).
+
+fail_catch_test() ->
+  Frequencies = dict:to_list( parallel_letter_frequency:dict(["asd",banana, "asd"]) ),
   ?assert( lists:sort(Frequencies) =:= [{$a,2},{$d,2},{$s,2}] ).

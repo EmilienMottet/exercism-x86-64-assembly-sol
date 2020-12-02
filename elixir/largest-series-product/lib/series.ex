f0a3a5ffb66f7b9551010ebe7b44b80625b09ff6
EmilienMottet <emilien.mottet@grenoble-inp.org>
2020-12-02 22:50:34 +0100

    refactor

--- elixir/largest-series-product/lib/series.ex
+++ elixir/largest-series-product/lib/series.ex
@@ -12,22 +12,26 @@ defmodule Series do
   end
 
   def largest_product(number_string, size) do
-    number_list = number_string |> String.to_charlist() |> Enum.map(fn x -> x - ?0 end)
+    number_list =
+      number_string
+      |> String.to_charlist()
+      |> Enum.map(&(&1 - ?0))
+
     {l1, l2} = number_list |> Enum.split(size)
-    do_largest_product(l1, l2, size, 0)
+    largest_product(l1, l2, size, 0)
   end
 
-  defp do_largest_product(l, [], _s, m) do
+  defp largest_product(l, [], _s, m) do
     p = do_product(l)
     max(p, m)
   end
 
-  defp do_largest_product(l = [_h | t1], [h | t2], s, m) do
+  defp largest_product(l = [_h | t1], [h | t2], s, m) do
     p = do_product(l)
-    do_largest_product(t1 ++ [h], t2, s, max(p, m))
+    largest_product(t1 ++ [h], t2, s, max(p, m))
   end
 
   defp do_product(l) do
-    l |> Enum.reduce(1, fn n, acc -> n * acc end)
+    l |> Enum.reduce(1, &(&1 * &2))
   end
 end

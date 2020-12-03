52bda0a467b5e3a88315e18e7e6fba29efe8e7ec
EmilienMottet <emilien.mottet@grenoble-inp.org>
2020-12-03 08:40:31 +0100

    refactor as previous anagram

--- ruby/anagram/anagram.rb
+++ ruby/anagram/anagram.rb
@@ -1,29 +1,22 @@
 class Anagram
   private
 
-  attr_accessor :given, :computed_given
+  attr_accessor :word, :computed_word
 
   def compute_word(word)
     word.downcase.chars.sort
   end
 
-  def normalized(words)
-    words = words.map { |w| [w, compute_word(w)] }.to_h
-    words.delete_if { |w, _n| w.casecmp(given).zero? }
-    words
-  end
-
   def initialize(word)
-    @given = word
-    @computed_given = compute_word(given)
+    @word = word
+    @computed_word = compute_word(word)
   end
 
   public
 
   def match(*words)
-    abecedarian = normalized(words.flatten)
-    abecedarian.select do |_w, n|
-      n == computed_given
-    end.keys
+    words.flatten.reject { |w| w.downcase == @word.downcase }.select do |w|
+      compute_word(w) == computed_word
+    end
   end
 end

f3d31a47e85f4633c62937fc9242735c6cb74a02
EmilienMottet <emilien.mottet@grenoble-inp.org>
2020-12-11 23:13:27 +0100

    use lea instead mov reg, []

--- pangram/pangram.asm
+++ pangram/pangram.asm
@@ -2,10 +2,10 @@ default rel
 section .text
 global is_pangram
 is_pangram:
-    mov r8d, [alphabet]         ; put alphabet in r8d
+    lea r8d, [rel alphabet]     ; put alphabet in r8d
 .while:
     cmp [rdi],byte 0            ; rdi have first param, the tested word
-    jz .end                      ; break while when '\0' is reached
+    jz .end                     ; break while when '\0' is reached 
     mov cl, [rdi]               ; read nth char of input
     sub cl, 'a'                 ; find index of char in frequency array
     mov eax, 1                  ; create word : 0b0{31}1
@@ -13,7 +13,7 @@ is_pangram:
     not eax                     ; create a word like : 0b1.*01.*
     and r8d, eax                ; set bit index at 0 in frequency array
     inc rdi                     ; inc for read next char
-    jmp .while                   ; go to begin of loop
+    jmp .while                  ; go to begin of loop
 .end:
     xor rax,rax                 ; ensure 0 on rax (clean byte at 8..63)
     test r8d, r8d               ; set ZF to 1 if r8d == 0

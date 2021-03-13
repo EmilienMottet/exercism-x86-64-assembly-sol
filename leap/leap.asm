48d747f4bcf627a8c0bc95dde4c04959299c102b
EmilienMottet <emilien.mottet@grenoble-inp.org>
2021-03-13 21:04:55 +0100

    optimise test order for leap
        75% of all years cannot be leap years because they are not multiples of 4; test year % 4 == 0 first
        98.97% of all leap years are multiples of 4 that are not multiples of 100; test year % 100 != 0 second
        1.03% of all leap years are multiples of 4 that are also multiples of 100 and 400; test year % 400 == 0 third

--- leap/leap.asm
+++ leap/leap.asm
@@ -1,15 +1,18 @@
 section .text
 global leap_year
 leap_year:
-    mov rsi, 400
+    ; year % 4 == 0
+    mov rsi, 4
     call modulo
     test rax, rax
-    jz is_leap
+    jnz is_no_leap
+    ; year % 100 != 0
     mov rsi, 100
     call modulo
     test rax, rax
-    jz is_no_leap
-    mov rsi, 4
+    jnz is_leap
+    ; year % 400 == 0
+    mov rsi, 400
     call modulo
     test rax, rax
     jz is_leap
@@ -17,7 +20,7 @@ is_no_leap:
     xor rax, rax
     ret
 is_leap:
-    setz al
+    mov al, 1
     ret
 
 modulo:
